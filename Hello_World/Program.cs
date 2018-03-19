a614f41195c458b2d6a17b12310d935296b824c4
Vatsal Patel <vatsalpb@gmail.com>
2018-03-19 15:18:00 -0500

    Add project files.

---
+++ Hello_World/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            decimal someVar;
+            someVar = 7;
+            Console.WriteLine("Print someVar: ", someVar);
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
